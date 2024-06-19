d0387a858acc5095afe966117d211b84e785a55e
sudarshanmu <sudarshan.muley.12@gmail.com>
2024-06-19 01:09:17 +0000

    feat: Updated the commands
    HS-33

--- shell.sh
+++ shell.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sed -i -E 's/("version": ")([0-9]+)\.([0-9]+)\.([0-9]+)"/echo "\1\2."$((${3} + 1))."\4"/e' app.json
-sed -i -E 's/("buildNumber": ")([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)"/echo "\1\2."$((${3} + 1))."\4.\5"/e' app.json
-
-#current_build_number=$(cat app.json | jq -r '.expo.ios.buildNumber')
-#new_build_number=$(echo $current_build_number | awk -F '.' '{print $1"."($2 + 1)"."$3"."$4}')
-#sed -i "s/\"buildNumber\": \".*\"/\"buildNumber\": \"$new_build_number\"/" app.json
+current_build_number=$(cat app.json | jq -r '.expo.ios.buildNumber')
+new_build_number=$(echo $current_build_number | awk -F '.' '{print $1"."($2 + 1)"."$3"."$4}')
+sed -i "s/\"buildNumber\": \".*\"/\"buildNumber\": \"$new_build_number\"/" app.json
