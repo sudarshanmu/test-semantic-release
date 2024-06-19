92110fb5b51e2e4badab86b1394a23057b3e09ac
sudarshanmu <sudarshan.muley.12@gmail.com>
2024-06-19 01:48:11 +0000

    feat: Updated the command
    TI-23

--- shell.sh
+++ shell.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 current_version=$(cat app.json | jq -r '.expo.version')
 new_version=$(echo $current_version | awk -F '.' '{print $1"."($2 + 1)"."$3}')
-sed -i "s/\"version\": \".*\"/\"version\": \"$new_version\"/" app.json
+jq --arg new_version "$new_version" '.expo.version = $new_version' app.json > tmp.json && mv tmp.json app.json
+#sed -i "s/\"version\": \".*\"/\"version\": \"$new_version\"/" app.json
 
 current_build_number=$(cat app.json | jq -r '.expo.ios.buildNumber')
 new_build_number=$(echo $current_build_number | awk -F '.' '{print $1"."($2 + 1)"."$3"."$4}')
