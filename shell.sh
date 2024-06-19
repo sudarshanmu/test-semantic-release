7477715a697863226af88e412f24ad2f81394f36
sudarshanmu <sudarshan.muley.12@gmail.com>
2024-06-19 03:43:48 +0000

    feat: Updated shell script
    HI-23

--- shell.sh
+++ shell.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-current_version=$(cat app.json | jq -r '.expo.version')
-new_version=$(echo $current_version | awk -F '.' '{print $1"."($2 + 1)"."$3}')
-jq --arg new_version "$new_version" '.expo.version = $new_version' app.json > tmp.json && mv tmp.json app.json
 
-current_build_number=$(cat app.json | jq -r '.expo.ios.buildNumber')
-new_build_number=$(echo $current_build_number | awk -F '.' '{print $1"."($2 + 1)"."$3"."$4}')
-sed -i "s/\"buildNumber\": \".*\"/\"buildNumber\": \"$new_build_number\"/" app.json
+release_type=$1
 
-current_marketing_version=$(cat eas.json | jq -r '.build.base.env.MARKETING_VERSION')
-new_marketing_version=$(echo $current_marketing_version | awk -F '.' '{print $1"."($2 + 1)"."$3}')
-jq --arg new_marketing_version "$new_marketing_version" '.build.base.env.MARKETING_VERSION = $new_marketing_version' eas.json > tmp.json && mv tmp.json eas.json
-#sed -i "s/\"MARKETING_VERSION\": \".*\"/\"MARKETING_VERSION\": \"$new_marketing_version\"/" eas.json
+if [ "$release_type" = "major" ]; then
+    echo "Performing actions for major release..."
+elif [ "$release_type" = "minor" ]; then
+    echo "Performing actions for minor release..."
+elif [ "$release_type" = "patch" ]; then
+    echo "Performing actions for patch release..."
+else
+    echo "Unknown release type: $release_type"
+fi
 
-old_version=$(cat package.json | jq -r '.version')
-latest_version=$(echo $old_version | awk -F '.' '{print $1"."($2 + 1)"."$3}')
-sed -i "s/\"version\": \".*\"/\"version\": \"$latest_version\"/" package.json
