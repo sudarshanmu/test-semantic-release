e27ea380a7547c8217c22037923b437959b1420d
sudarshanmu <sudarshan.muley.12@gmail.com>
2024-06-19 04:01:07 +0000

    feat: Updated shell script
    HU-123

--- shell.sh
+++ shell.sh
@@ -3,21 +3,21 @@
 release_type=$1
 
 if [ "$release_type" = "minor" ]; then
-  echo "Performing actions for major release..."
+  echo "Performing actions for minor release..."
   current_version=$(cat app.json | jq -r '.expo.version')
-  new_version=$(echo $current_version | awk -F '.' '{print ($1 + 1)"."0"."0}')
+  new_version=$(echo $current_version | awk -F '.' '{print $1"."($2 + 1)"."$3}')
   jq --arg new_version "$new_version" '.expo.version = $new_version' app.json > tmp.json && mv tmp.json app.json
 
   current_build_number=$(cat app.json | jq -r '.expo.ios.buildNumber')
-  new_build_number=$(echo $current_build_number | awk -F '.' '{print ($1 + 1)"."0"."0"."0}')
+  new_build_number=$(echo $current_build_number | awk -F '.' '{print $1"."($2 + 1)"."$3"."$4}')
   sed -i "s/\"buildNumber\": \".*\"/\"buildNumber\": \"$new_build_number\"/" app.json
 
   current_marketing_version=$(cat eas.json | jq -r '.build.base.env.MARKETING_VERSION')
-  new_marketing_version=$(echo $current_marketing_version | awk -F '.' '{print ($1 + 1)"."0"."0}')
+  new_marketing_version=$(echo $current_marketing_version | awk -F '.' '{print $1"."($2 + 1)"."$3}')
   jq --arg new_marketing_version "$new_marketing_version" '.build.base.env.MARKETING_VERSION = $new_marketing_version' eas.json > tmp.json && mv tmp.json eas.json
 
   old_version=$(cat package.json | jq -r '.version')
-  latest_version=$(echo $old_version | awk -F '.' '{print ($1 + 1)"."0"."0}')
+  latest_version=$(echo $old_version | awk -F '.' '{print $1"."($2 + 1)"."$3}')
   sed -i "s/\"version\": \".*\"/\"version\": \"$latest_version\"/" package.json
 else
   echo "Unknown release type: $release_type"
