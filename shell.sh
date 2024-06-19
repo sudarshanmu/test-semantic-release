2f459a18ccca13752df1cfbe6c71b3afae2115e5
sudarshanmu <sudarshan.muley.12@gmail.com>
2024-06-19 08:56:08 +0000

    feat: Updated Shell script
    UT-334

--- shell.sh
+++ shell.sh
@@ -2,23 +2,77 @@
 
 release_type=$1
 
-if [ "$release_type" = "minor" ]; then
-  echo "Performing actions for patch release..."
-  current_version=$(cat app.json | jq -r '.expo.version')
-  new_version=$(echo $current_version | awk -F '.' '{print $1"."$2"."($3 + 1)}')
-  jq --arg new_version "$new_version" '.expo.version = $new_version' app.json > tmp.json && mv tmp.json app.json
+update_version() {
+  local file=$1
+  local jq_filter=$2
+  local new_version=$3
+  jq --arg new_version "$new_version" "$jq_filter" "$file" > tmp.json && mv tmp.json "$file"
+}
 
-  current_build_number=$(cat app.json | jq -r '.expo.ios.buildNumber')
-  new_build_number=$(echo $current_build_number | awk -F '.' '{print $1"."$2"."$3"."($4 + 1)}')
-  sed -i "s/\"buildNumber\": \".*\"/\"buildNumber\": \"$new_build_number\"/" app.json
+update_build_number() {
+  local file=$1
+  local new_build_number=$2
+  sed -i "s/\"buildNumber\": \".*\"/\"buildNumber\": \"$new_build_number\"/" "$file"
+}
 
-  current_marketing_version=$(cat eas.json | jq -r '.build.base.env.MARKETING_VERSION')
-  new_marketing_version=$(echo $current_marketing_version | awk -F '.' '{print $1"."$2"."($3 + 1)}')
-  jq --arg new_marketing_version "$new_marketing_version" '.build.base.env.MARKETING_VERSION = $new_marketing_version' eas.json > tmp.json && mv tmp.json eas.json
+increment_version() {
+  local version=$1
+  local part=$2
+  local IFS='.'
+  read -r major minor patch extra <<< "$version"
+  case $part in
+    major)
+      echo "$((major + 1)).0.0"
+      ;;
+    minor)
+      echo "$major.$((minor + 1)).$patch"
+      ;;
+    patch)
+      echo "$major.$minor.$((patch + 1))"
+      ;;
+  esac
+}
 
-  old_version=$(cat package.json | jq -r '.version')
-  latest_version=$(echo $old_version | awk -F '.' '{print $1"."$2"."($3 + 1)}')
-  sed -i "s/\"version\": \".*\"/\"version\": \"$latest_version\"/" package.json
-else
-  echo "Unknown release type: $release_type"
-fi
+increment_build_number() {
+  local build_number=$1
+  local part=$2
+  local IFS='.'
+  read -r major minor patch extra <<< "$build_number"
+  case $part in
+    major)
+      echo "$((major + 1)).0.0.0"
+      ;;
+    minor)
+      echo "$major.$((minor + 1)).$patch.$extra"
+      ;;
+    patch)
+      echo "$major.$minor.$patch.$((extra + 1))"
+      ;;
+  esac
+}
+
+case $release_type in
+  major | minor | patch)
+    echo "Performing actions for $release_type release..."
+
+    current_version=$(jq -r '.expo.version' app.json)
+    new_version=$(increment_version "$current_version" "$release_type")
+    update_version app.json '.expo.version = $new_version' "$new_version"
+
+    current_build_number=$(jq -r '.expo.ios.buildNumber' app.json)
+    new_build_number=$(increment_build_number "$current_build_number" "$release_type")
+    update_build_number app.json "$new_build_number"
+
+    current_marketing_version=$(jq -r '.build.base.env.MARKETING_VERSION' eas.json)
+    new_marketing_version=$(increment_version "$current_marketing_version" "$release_type")
+    update_version eas.json '.build.base.env.MARKETING_VERSION = $new_marketing_version' "$new_marketing_version"
+
+    old_version=$(jq -r '.version' package.json)
+    latest_version=$(increment_version "$old_version" "$release_type")
+    update_version package.json '.version = $new_version' "$latest_version"
+    ;;
+  *)
+    echo "Unknown release type: $release_type"
+    ;;
+esac
+
