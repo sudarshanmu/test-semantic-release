d480b4f7c76960e101df8ce124f942ffcf60e024
sudarshanmu <sudarshan.muley.12@gmail.com>
2024-06-19 00:58:34 +0000

    feat: Added one command instead of 3
    HS-123

--- shell.sh
+++ shell.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-current_build_number=$(cat app.json | jq -r '.expo.ios.buildNumber')
-new_build_number=$(echo $current_build_number | awk -F '.' '{print $1"."($2 + 1)"."$3"."$4}')
-sed -i "s/\"buildNumber\": \".*\"/\"buildNumber\": \"$new_build_number\"/" app.json
+sed -i -E 's/("buildNumber": ")([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)"/echo "\1\2."$((${3} + 1))."\4.\5"/e' app.json
+
+#current_build_number=$(cat app.json | jq -r '.expo.ios.buildNumber')
+#new_build_number=$(echo $current_build_number | awk -F '.' '{print $1"."($2 + 1)"."$3"."$4}')
+#sed -i "s/\"buildNumber\": \".*\"/\"buildNumber\": \"$new_build_number\"/" app.json
