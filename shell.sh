76375625d9d9c7ba04ffaf7a2235b747f8e18bcf
sudarshanmu <sudarshan.muley.12@gmail.com>
2024-06-19 01:38:43 +0000

    feat: Added few more commands to replace version
    TI-1800

--- shell.sh
+++ shell.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+current_version=$(cat app.json | jq -r '.expo.version')
+new_version=$(echo $current_version | awk -F '.' '{print $1"."($2 + 1)"."$3}')
+sed -i "s/\"version\": \".*\"/\"version\": \"$new_version\"/" app.json
+
 current_build_number=$(cat app.json | jq -r '.expo.ios.buildNumber')
 new_build_number=$(echo $current_build_number | awk -F '.' '{print $1"."($2 + 1)"."$3"."$4}')
 sed -i "s/\"buildNumber\": \".*\"/\"buildNumber\": \"$new_build_number\"/" app.json
+
